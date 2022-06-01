7be647af8de930374a0e69b5ea915de4e928a764
pedroigosl <pedroigosl@gmail.com>
2022-06-01 18:34:39 -0300

    feat: added recording to capture object. Not yet made an endpoint

--- main.py
+++ main.py
@@ -17,25 +17,35 @@ class capture():
 
     def __init__(self, name, show_img=False, recording=False, cam_id=0):
         if name == None:
-            self.name = f"video #{count}"
+            self.name = f"video #{count}.mp4"
         else:
-            self.name = name
+            self.name = name + '.mp4'
         self.recording = recording
         self.show_img = show_img
         self.cam_id = cam_id
-        self.cap = cv2.VideoCapture(self.cam_id)
         self.close = False
+
+        self.cap = cv2.VideoCapture(self.cam_id)
+        self.width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        self.writer = None
 
     def start(self):
         while True:
-            video_name = self.name + '.mp4'
+            title = self.name
             _, frame = self.cap.read()
             if self.show_img:
                 cv2.imshow('video', frame)
-                cv2.setWindowTitle('video', video_name)
+                cv2.setWindowTitle('video', title)
             if (cv2.waitKey(1) == 27) or self.close:
                 break
+
+            if self.recording == True:
+                self.writer.write(frame)
         self.cap.release()
+        if self.recording == True:
+            self.writer.release()
         cv2.destroyAllWindows()
 
     def rename(self, new_name):
@@ -43,6 +53,14 @@ class capture():
             print("Already recording, name not changed")
             return
         self.name = new_name
+
+    def record(self, new_name=None):
+        if new_name:
+            self.rename(new_name)
+        self.recording = True
+        self.writer = cv2.VideoWriter(
+            self.name, cv2.VideoWriter_fourcc(*'mp4v'), 20,
+            (self.width, self.height))
 
     def stop(self):
         self.close = True
